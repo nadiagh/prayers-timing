05a12c5019732c7026f176db37291abc8271725d
nadia ghedir <nadoudanadiaghedirnadouda@outlook.fr>
2018-11-02 10:55:04 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            time:[]
+        }
+    }
+
+    componentDidMount(){
+
+        fetch('http://api.aladhan.com/v1/timingsByCity?city=Mahdia&country=Tunisia&method=1&midnightMode=1')
+            .then(response=>response.json())
+            .then(data=>{
+
+                const arr=Object.entries(data.data.timings);
+                this.setState({time:arr})
+                 console.log(this.state.time)
+
+            });
+
+        }
+
+
+
+
+
+   
+  
+
+  render() {
+    return (
+      <div className="App" style={{overflowY:"scroll"}}>
+
+          {this.state.time.map(value=>{
+              return(
+                  <div className="prayer">
+                  <p className="transbox" key={value[0]}>{value[0]}</p>
+                  <p className="transbox" key={value[0]}>{value[1]}</p>
+                  </div>
+              );
+          }  )}
+          </div>
+    );
+  }
+}
+
+export default App;
